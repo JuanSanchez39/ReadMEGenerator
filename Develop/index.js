2678872e4c38ae6fe3d093abb8349eb75f2997ad
JuanSanchez39 <juan_sanchez39@hotmail.com>
2023-06-15 18:28:01 -0400

    changes missing

--- Develop/index.js
+++ Develop/index.js
@@ -7,60 +7,62 @@ const generateMarkdown = require('./generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
     {
-      type: 'input',
-      name: 'title',
-      message: 'Enter the title of your project:',
+        type: 'input',
+        name: 'title',
+        message: 'Enter the title of your project:',
     },
     {
-      type: 'input',
-      name: 'description',
-      message: 'Enter a description of your project:',
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of your project:',
     },
     {
-      type: 'input',
-      name: 'installation',
-      message: 'Enter installation instructions:',
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions:',
     },
     {
-      type: 'input',
-      name: 'usage',
-      message: 'Enter usage information:',
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information:',
     },
     {
-      type: 'input',
-      name: 'contributing',
-      message: 'Enter contributing guidelines:',
+        type: 'input',
+        name: 'contributing',
+        message: 'Enter contribution guidelines:',
+    },
+
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter test instructions:',
     },
     {
-      type: 'input',
-      name: 'tests',
-      message: 'Enter test instructions:',
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', '', '', '', 'None'],
     },
     {
-      type: 'input',
-      name: 'username',
-      message: 'Enter your GitHub username:',
+        type: 'input',
+        name: 'username',
+        message: 'Enter your GitHub username:',
     },
     {
-      type: 'input',
-      name: 'email',
-      message: 'Enter your email address:',
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
     },
-    {
-      type: 'list',
-      name: 'license',
-      message: 'Choose a license for your project:',
-      choices: ['MIT', '', '', '', 'None'],
-    },
-  ];
-  
-  
+
+];
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
